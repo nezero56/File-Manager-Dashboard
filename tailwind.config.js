5d255f87f8ccb8238ceead7efe9ea30bb23acace
nezero56 <munezerosonia85@gmail.com>
2026-06-22 23:13:17 +0200

    feat: configure Tailwind CSS and design tokens

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3A57E8',
+        info: '#08B1BA',
+        pageBg: '#F5F6FA',
+      },
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
